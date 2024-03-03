6da46190321ca8a09a076ad83b99f3938bfa07a5
Kumar Dhale <99580094+kumardhale@users.noreply.github.com>
2024-03-04 04:43:50 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-Kumar"
+    bucket = "mydev-tf-state-bucket-project-terraform-kumar"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
